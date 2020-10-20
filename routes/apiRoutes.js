5a8df39db39f4f29e0b6139b3e88a26dfc9fa441
Alex Varela <alexvar93@gmail.com>
2020-10-20 16:42:18 -0400

    server is now functional with working html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    
+});
+
+router.get("/api/workouts/:id", (req, res) => {
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Defining out HTML and API routes to use
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}!`);
+});
